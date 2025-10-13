6b4b35de342a3dbb5ca189360b5a589f6e04fe8f
Derek Boucher <derek.a.boucher@gmail.com>
2025-10-13 15:06:51 -0400

    added intro section

--- soen357/mini_project/src/App.tsx
+++ soen357/mini_project/src/App.tsx
@@ -1,9 +1,11 @@
+import IntroSection from "./sections/Intro";
 import TitleSection from "./sections/Title";
 
 export function App() {
   return (
     <div className="app font-inter">
       <TitleSection />
+      <IntroSection />
     </div>
   );
 }
