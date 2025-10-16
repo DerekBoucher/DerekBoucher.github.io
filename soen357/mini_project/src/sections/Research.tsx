4adbf2c9198de397e59087163b15f4e9530c820a
Derek Boucher <derek.a.boucher@gmail.com>
2025-10-15 21:48:15 -0400

    styled the research a bit

--- soen357/mini_project/src/sections/Research.tsx
+++ soen357/mini_project/src/sections/Research.tsx
@@ -6,6 +6,7 @@ import survey5 from "@/assets/survey_5.png";
 import survey6 from "@/assets/survey_6.png";
 import survey7 from "@/assets/survey_7.png";
 import survey8 from "@/assets/survey_8.png";
+import persona1 from "@/assets/persona_1.png";
 
 export function ResearchSection() {
     return (
@@ -17,21 +18,58 @@ export function ResearchSection() {
                     First and foremost, it is important to get a sense of what our
                     potential users think is a major blocker when it comes to trust in AI
                     products. What are features that an app can have that would change
-                    their mind on AI reliability? The common pitfalls they encounter?
+                    their mind on AI reliability? What are the common pitfalls they
+                    encounter?
                 </p>
                 <p className="text-lg mt-5">
-                    To get some introspection into this, a survey was sent out to a group
-                    of participants from various backgrounds.
+                    To get some insight into this, a survey was sent out to a group of
+                    participants from various backgrounds.
                 </p>
                 <h2 className="text-2xl mt-5 underline">Survey</h2>
-                <img className="mt-5" src={survey1} alt="survey_question_1" />
-                <img className="mt-5" src={survey2} alt="survey_question_2" />
-                <img className="mt-5" src={survey3} alt="survey_question_3" />
-                <img className="mt-5" src={survey4} alt="survey_question_4" />
-                <img className="mt-5" src={survey5} alt="survey_question_5" />
-                <img className="mt-5" src={survey6} alt="survey_question_6" />
+                <div className="mt-5 flex flex-col bg-white p-6 rounded-lg border-[#00000054] border shadow-md">
+                    <div className="flex flex-row">
+                        <img
+                            className="mt-5 mr-5 w-1/2"
+                            src={survey1}
+                            alt="survey_question_1"
+                        />
+                        <img className="mt-5 w-1/2" src={survey2} alt="survey_question_2" />
+                    </div>
+                    <p className="text-lg mt-5">
+                        The first set of questions we're geared towards knowing how familiar
+                        our sample is with AI tools. This can give us a sense of how
+                        conservative or progressive the design should be.
+                    </p>
+                    <p className="text-lg mt-5">
+                        The concensus seemed to be that the majority of them are{" "}
+                        <b>
+                            familiar with AI and use it in some moderate capacity in a
+                            day-to-day basis.
+                        </b>
+                    </p>
+                </div>
+                <div className="flex flex-col justify-center bg-white p-5 mt-5 rounded-lg border-[#00000054] border shadow-sm">
+                    <div className="flex flex-row justify-center">
+                        <img
+                            className="mt-5 w-[75%]"
+                            src={survey3}
+                            alt="survey_question_3"
+                        />
+                    </div>
+                    <div className="flex flex-row justify-center">
+                        <img
+                            className="mt-5 mr-5 w-1/2"
+                            src={survey4}
+                            alt="survey_question_4"
+                        />
+                        <img className="mt-5 w-1/2" src={survey6} alt="survey_question_6" />
+                    </div>
+                </div>
+
                 <img className="mt-5" src={survey7} alt="survey_question_7" />
                 <img className="mt-5" src={survey8} alt="survey_question_8" />
+                <h2 className="text-2xl mt-5 underline">Personas</h2>
+                <img className="mt-5" src={persona1} alt="persona_1" />
             </div>
             <div className="flex-1"></div>
         </div>
