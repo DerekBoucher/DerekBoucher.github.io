5d5ba5aee6de1374816181fe11ea7738cb4180e8
Derek Boucher <derek.a.boucher@gmail.com>
2025-10-16 22:31:48 -0400

    added ai hero

--- soen357/mini_project/src/sections/Title.tsx
+++ soen357/mini_project/src/sections/Title.tsx
@@ -1,12 +1,17 @@
 import FadeSection from "../components/FadeSection";
+import hero from "@/assets/ai_hero.jpg";
 
 export function TitleSection() {
     return (
-        <div className="h-[1000px] bg-blue-100">
+        <div className="h-[1000px]">
             <div className="h-32 bg-blue-600 font-extralight pl-10 flex items-center text-white text-4xl">
                 SOEN357: Mini Project - Derek Boucher
             </div>
-            <div className="flex flex-row items-center h-[100%] justify-center">
+            <div
+                className={
+                    "flex flex-row items-center h-[100%] justify-center bg-cover"
+                }
+            >
                 <div className="bg-blue-600 w-[25px] h-32 mr-3"></div>
                 <FadeSection>
                     <div className="flex flex-col">
--- soen357/mini_project/src/types/assets.d.ts
+++ soen357/mini_project/src/types/assets.d.ts
@@ -2,6 +2,10 @@ declare module "*.png" {
     const src: string;
     export default src;
 }
+declare module "*.jpg" {
+    const src: string;
+    export default src;
+}
 
 declare module "*.svg" {
     const src: string;
